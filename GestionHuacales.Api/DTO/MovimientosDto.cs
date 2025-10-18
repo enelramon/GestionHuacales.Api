65b152cc304cf82bd06490b7f953032d406d8fda
Esthiber <esthiber1@gmail.com>
2025-10-18 18:23:08 -0400

    Se creo MovimientosDto y se implemento en MovimientosController 'GET'

---
+++ GestionHuacales.Api/DTO/MovimientosDto.cs
@@ -0,0 +1,18 @@
+﻿using GestionHuacales.Api.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionHuacales.Api.DTO
+{
+    public class MovimientosDto
+    {
+        public int PartidaId { get; set; }
+
+        public string Jugador { get; set; } = "X";
+
+        public int PosicionFila { get; set; }
+
+        public int PosicionColumna { get; set; }
+                
+    }
+}
