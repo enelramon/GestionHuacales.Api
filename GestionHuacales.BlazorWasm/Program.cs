67daa27b7759c67180fd46dd2d20961e8605b937
Enel Almonte <enelramon@hotmail.com>
2025-10-21 22:25:01 -0400

    Enhance TicTacToe game functionality and error handling
    - Added IMovimientosApiService for handling game movements.
    - Updated UI to allow players to select symbols (X or O) before joining.
    - Improved error handling and refactored game state management to use async methods.
    - Changed HTTP client base address to local development URL in Program.cs.
    - Introduced ResourceExtensions.cs for streamlined resource handling.

--- GestionHuacales.BlazorWasm/Program.cs
+++ GestionHuacales.BlazorWasm/Program.cs
@@ -5,10 +5,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.Add<HeadOutlet>("head::after");//https://gestionhuacalesapi.azurewebsites.net/
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7047/") });
 builder.Services.AddScoped<ITareasApiService, TareasApiService>();
 builder.Services.AddScoped<IPartidasApiService, PartidasApiService>();
+builder.Services.AddScoped<IMovimientosApiService, MovimientosApiService>();
 
 await builder.Build().RunAsync();
